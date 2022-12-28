b26bf6acd0dca082204e0083a5a23e1c2dea53f8
Ethan Peterson <17emp5@queensu.ca>
2022-12-28 10:28:59 -0500

    Make Module Indentation Consistent Across RTL Folder (#49)

--- rtl/common/xadc_bfm.sv
+++ rtl/common/xadc_bfm.sv
@@ -44,87 +44,87 @@ module xadc_bfm (
     output var logic busy_out // busy flag indicating a conversion in progress
 );
 
-typedef enum int {
-    INIT,
-    IDLE,
-    PULSE_EOS,
-    AWAIT_READ_REQUEST,
-    READ_WAIT_CYCLE, // DRP will not have data right away so we will waste a cycle here
-    PREP_READ_DATA, // Select the correct output data based on the address input
-    SHOW_READ_DATA
-} xadc_bfm_state_t;
+    typedef enum int {
+        INIT,
+        IDLE,
+        PULSE_EOS,
+        AWAIT_READ_REQUEST,
+        READ_WAIT_CYCLE, // DRP will not have data right away so we will waste a cycle here
+        PREP_READ_DATA, // Select the correct output data based on the address input
+        SHOW_READ_DATA
+    } xadc_bfm_state_t;
 
-xadc_bfm_state_t state;
+    xadc_bfm_state_t state;
 
-// Internal registers for fake data samples
-var logic[15:0] vaux4_conv;
-var logic[15:0] vaux12_conv;
-var logic[15:0] conv_value; // Changes between vaux4 and vaux12 depending on address given.
+    // Internal registers for fake data samples
+    var logic[15:0] vaux4_conv;
+    var logic[15:0] vaux12_conv;
+    var logic[15:0] conv_value; // Changes between vaux4 and vaux12 depending on address given.
 
-always_ff @(posedge dclk_in) begin
-    if (reset_in) begin
-        state <= INIT;
-    end else begin
-        case (state)
-            INIT: begin
-                // Set initial states for outputs and then transition into
-                // operating mode
-                drdy_out <= 0;
-                do_out <= 16'h00_00;
-                channel_out <= 5'b00000;
-                eoc_out <= 0;
-                eos_out <= 0;
-                alarm_out <= 0;
-                busy_out <= 0;
+    always_ff @(posedge dclk_in) begin
+        if (reset_in) begin
+            state <= INIT;
+        end else begin
+            case (state)
+                INIT: begin
+                    // Set initial states for outputs and then transition into
+                    // operating mode
+                    drdy_out <= 0;
+                    do_out <= 16'h00_00;
+                    channel_out <= 5'b00000;
+                    eoc_out <= 0;
+                    eos_out <= 0;
+                    alarm_out <= 0;
+                    busy_out <= 0;
 
-                // Set internal registers
-                vaux12_conv <= 255;
-                vaux4_conv <= 127;
-                conv_value <= 0;
+                    // Set internal registers
+                    vaux12_conv <= 255;
+                    vaux4_conv <= 127;
+                    conv_value <= 0;
 
-                // Proceed to next state
-                state <= IDLE;
-            end
-            IDLE: begin
-                // set eos pulse for the next cycle and transition
-                eos_out <= 1;
-                state <= PULSE_EOS;
-            end
-            PULSE_EOS: begin
-                // Hold the EOS high for one cycle
-                eos_out <= 0;
-                state <= AWAIT_READ_REQUEST;
-            end
-            AWAIT_READ_REQUEST: begin
-                if (den_in) begin
-                    // check the address provided and load the conversion reg
-                    // TODO: These addresses should come from an include
-                    if (daddr_in == 7'h14) begin
-                        // if we are reading the current sensor
-                        conv_value <= vaux4_conv;
-                    end else if (daddr_in <= 7'h1c) begin
-                        conv_value <= vaux12_conv;
-                    end else begin
-                        $display("Unknown Address provided");
-                    end 
-                    state <= READ_WAIT_CYCLE;
+                    // Proceed to next state
+                    state <= IDLE;
                 end
-            end 
-            READ_WAIT_CYCLE: state <= PREP_READ_DATA;
-            PREP_READ_DATA: begin
-                // Set ready and data_output signals
-                drdy_out <= 1;
-                do_out <= conv_value;
-                state <= SHOW_READ_DATA;
-            end
-            SHOW_READ_DATA: begin
-                drdy_out <= 0;
-                do_out <= 0;
-                state <= IDLE;
-            end
-        endcase
+                IDLE: begin
+                    // set eos pulse for the next cycle and transition
+                    eos_out <= 1;
+                    state <= PULSE_EOS;
+                end
+                PULSE_EOS: begin
+                    // Hold the EOS high for one cycle
+                    eos_out <= 0;
+                    state <= AWAIT_READ_REQUEST;
+                end
+                AWAIT_READ_REQUEST: begin
+                    if (den_in) begin
+                        // check the address provided and load the conversion reg
+                        // TODO: These addresses should come from an include
+                        if (daddr_in == 7'h14) begin
+                            // if we are reading the current sensor
+                            conv_value <= vaux4_conv;
+                        end else if (daddr_in <= 7'h1c) begin
+                            conv_value <= vaux12_conv;
+                        end else begin
+                            $display("Unknown Address provided");
+                        end 
+                        state <= READ_WAIT_CYCLE;
+                    end
+                end 
+                READ_WAIT_CYCLE: state <= PREP_READ_DATA;
+                PREP_READ_DATA: begin
+                    // Set ready and data_output signals
+                    drdy_out <= 1;
+                    do_out <= conv_value;
+                    state <= SHOW_READ_DATA;
+                end
+                SHOW_READ_DATA: begin
+                    drdy_out <= 0;
+                    do_out <= 0;
+                    state <= IDLE;
+                end
+            endcase
+        end
     end
-end
 
 endmodule
 
--- rtl/ftdi_sync/reset_sync.sv
+++ rtl/ftdi_sync/reset_sync.sv
@@ -6,19 +6,19 @@ module reset_sync (
     output logic reset_out
 );
 
-xpm_cdc_sync_rst #(
-   .DEST_SYNC_FF(4),   // DECIMAL; range: 2-10
-   .INIT(1),           // DECIMAL; 0=initialize synchronization registers to 0, 1=initialize synchronization
-                       // registers to 1
-   .INIT_SYNC_FF(0),   // DECIMAL; 0=disable simulation init values, 1=enable simulation init values
-   .SIM_ASSERT_CHK(0)  // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
-)
-xpm_cdc_sync_rst_inst (
-   .dest_rst(reset_out), // 1-bit output: src_rst synchronized to the destination clock domain. This output
-                        // is registered.
+   xpm_cdc_sync_rst #(
+      .DEST_SYNC_FF(4),   // DECIMAL; range: 2-10
+      .INIT(1),           // DECIMAL; 0=initialize synchronization registers to 0, 1=initialize synchronization
+                        // registers to 1
+      .INIT_SYNC_FF(0),   // DECIMAL; 0=disable simulation init values, 1=enable simulation init values
+      .SIM_ASSERT_CHK(0)  // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+   )
+   xpm_cdc_sync_rst_inst (
+      .dest_rst(reset_out), // 1-bit output: src_rst synchronized to the destination clock domain. This output
+                           // is registered.
 
-   .dest_clk(destination_clk), // 1-bit input: Destination clock.
-   .src_rst(reset_in)    // 1-bit input: Source reset signal.
-);
+      .dest_clk(destination_clk), // 1-bit input: Destination clock.
+      .src_rst(reset_in)    // 1-bit input: Source reset signal.
+   );
 
 endmodule
