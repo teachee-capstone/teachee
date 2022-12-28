03ad1992fb519abfa193142d45292b5a218392cb
Ethan Peterson <17emp5@queensu.ca>
2022-12-27 23:28:49 -0500

    prep the example for merging

--- rtl/xadc/xadc.sv
+++ rtl/xadc/xadc.sv
@@ -1,21 +1,6 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-// Notes from Chat with alex:
-// Transform constants to a package import
-// Use native FTDI packet size of 512 bytes for optimal performance
-// In sw, always read 512 bytes (this reads half the double 1k buf)
-// This project needs a refactor in all state names. Let's do an example
-// The FTDI init state for example would become FTDI_SLAVE_STATE_INIT
-// These prefixes make state machines easy to CTRL F and differentiate from other modules
-// use '0 to expand to zero out a whole port / vec, should refactor accordingly for this
-// Up the sys_clk to 100 MHz with a PLL Critical since chip wasn't flashing properly due to ratio between sys_clk and JTAG clock. Had to manually lower JTAG clock
-// Get rid of incremental compiles on all projects
-// NOTE: consider git adding xci file since it holds the ila / IP config (double check this) also needs to be re synthed on the users machine
-// Indent everything in the module
-// Switch to KDE
-
-
 // Tutorial used to generate the ADC IP core from Xilinx: https://www.youtube.com/watch?v=2j4UHLYqBDI
 // My only change is using channels 4 and 12 to correspond to our board design
 // Also fed an input clock of 12 MHz for now. Will need 100MHz for full 1MSPS. Right now we make 230 KSPS
@@ -99,11 +84,6 @@ xadc_wiz_0 xadc_wiz_cmod_inst (
   .eos_out(xadc_eos),          // output wire eos_out
   .busy_out()        // output wire busy_out
 );
-// Basic idea is that xadc_eos should go high to indicate end of sequence.
-// Once end of sequence is detected, the FPGA will initiate a read from each channel. (Use the addresses defined at top of file)
-// The data out from the DRP interface will be stored in either the current or voltage reading register (depending on address value)
-// Each value is 16 bits so we will combine them into one 32 bit async FIFO axi output. Then in future, the full scope design can consume
-// those samples and stream them into the ft232h module as bytes using a cobs encoder.
 
 // Programmer data input side wires
 axis_io sys_axis (
@@ -147,6 +127,7 @@ always_ff @(posedge sys_clk) begin
         AWAIT_ADC_CONV: begin
             if (xadc_eos) begin
                 // set the signals to start the DRP read
+                // NOTE: change this to CURRENT_ADDR if you want to read the current probe
                 xadc_daddr <= XADC_DRP_ADDR_VOLTAGE_CHANNEL;
                 xadc_den <= 1;
 
@@ -160,7 +141,7 @@ always_ff @(posedge sys_clk) begin
         AWAIT_DRP_DATA: begin
             if (xadc_drdy) begin
                 // grab upper 8 bits of the conversion
-                sys_axis.tdata <= xadc_do[15:8];
+                sys_axis.tdata <= xadc_do[11:4];
                 sys_axis.tvalid <= 1;
 
                 state <= SEND_TO_HOST;
@@ -175,27 +156,28 @@ always_ff @(posedge sys_clk) begin
     endcase
 end
 
-ila_0 xadc_fsm_ila (
-	.clk(sys_clk), // input wire clk
+// Useful ILA for checking this FSM
+// ila_0 xadc_fsm_ila (
+// 	.clk(sys_clk), // input wire clk
 
 
-	.probe0(state), // input wire [31:0]  probe0  
-	.probe1({sys_axis.tready, sys_axis.tvalid}), // input wire [31:0]  probe1 
-	.probe2(xadc_do), // input wire [31:0]  probe2 
-	.probe3(xadc_drdy), // input wire [31:0]  probe3 
-	.probe4(xadc_den), // input wire [31:0]  probe4 
-	.probe5(xadc_daddr), // input wire [31:0]  probe5 
-	.probe6(xadc_eos), // input wire [31:0]  probe6 
-	.probe7('0), // input wire [31:0]  probe7 
-	.probe8('0), // input wire [31:0]  probe8 
-	.probe9('0), // input wire [31:0]  probe9 
-	.probe10('0), // input wire [31:0]  probe10 
-	.probe11('0), // input wire [31:0]  probe11 
-	.probe12('0), // input wire [31:0]  probe12 
-	.probe13('0), // input wire [31:0]  probe13 
-	.probe14('0), // input wire [31:0]  probe14 
-	.probe15('0) // input wire [31:0]  probe15
-);
+// 	.probe0(state), // input wire [31:0]  probe0  
+// 	.probe1({sys_axis.tready, sys_axis.tvalid}), // input wire [31:0]  probe1 
+// 	.probe2(xadc_do), // input wire [31:0]  probe2 
+// 	.probe3(xadc_drdy), // input wire [31:0]  probe3 
+// 	.probe4(xadc_den), // input wire [31:0]  probe4 
+// 	.probe5(xadc_daddr), // input wire [31:0]  probe5 
+// 	.probe6(xadc_eos), // input wire [31:0]  probe6 
+// 	.probe7('0), // input wire [31:0]  probe7 
+// 	.probe8('0), // input wire [31:0]  probe8 
+// 	.probe9('0), // input wire [31:0]  probe9 
+// 	.probe10('0), // input wire [31:0]  probe10 
+// 	.probe11('0), // input wire [31:0]  probe11 
+// 	.probe12('0), // input wire [31:0]  probe12 
+// 	.probe13('0), // input wire [31:0]  probe13 
+// 	.probe14('0), // input wire [31:0]  probe14 
+// 	.probe15('0) // input wire [31:0]  probe15
+// );
 
 endmodule
 
