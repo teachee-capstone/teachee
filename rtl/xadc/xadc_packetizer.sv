882c698cb4d68b09c466a3dedb1fdcf711abf657
Ethan Peterson <17emp5@queensu.ca>
2023-01-28 11:33:50 -0500

    save changes on packetizer

--- rtl/xadc/xadc_packetizer.sv
+++ rtl/xadc/xadc_packetizer.sv
@@ -1,5 +1,8 @@
 `default_nettype none
 `timescale 1ns / 1ps
+import xadc_drp_package::*;
+import xadc_packet_package::*;
+
 // This module will consume data from the XADC Channel FIFOs. The module will
 // convert the two 16 bit streams into a single 8 bit stream. The 8 bit stream
 // will contain header bytes to indicate what channel the sample is coming from.
@@ -27,6 +30,20 @@ module xadc_packetizer (
         XADC_PACKETIZER_LOAD_LOWER_CURRENT_BYTE,
         XADC_PACKETIZER_AWAIT_SAMPLES
     } xadc_packetizer_state_t;
+
+    xadc_packetizer_state_t state = XADC_PACKETIZER_INIT;
+
+    // Note: we are assuming that the voltage and current channel clocks are identical. Not an async module
+    // Also, the XADC axis is already FIFO'd on one side so we will not repeat FIFO instances here
+
+    always_ff @(posedge voltage_channel.clk) begin
+        case (state)
+            XADC_PACKETIZER_INIT: begin
+                // Select who has data available and transition into the loading state.
+                // if neither stream has data, await data instead
+            end
+        endcase
+    end
 endmodule
 
 `default_nettype none
