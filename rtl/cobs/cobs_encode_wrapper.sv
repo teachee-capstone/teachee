68e903720e2fa9d88555f57ff1554fd627217c1c
Ethan Peterson <17emp5@queensu.ca>
2023-01-28 19:18:45 -0500

    COBS Encoder Wrapper (#74)
    * start work on a wrapper for cobs encode module
    
    * start skeleton of a testbench, see if it passes CI
    
    * play around with test case syntax
    
    * completed primitive cobs encoder tb
    
    * rename test case
    
    * save changes before refactor
    
    * starting to work out a procedural method
    
    * missed the clocks facepalm
    
    * update actions to pull in submodules

---
+++ rtl/cobs/cobs_encode_wrapper.sv
@@ -0,0 +1,29 @@
+`default_nettype none
+`timescale 1ns / 1ps
+// Wrapper module to fit the cobs encoder library module to our interface
+
+module cobs_encode_wrapper (
+    axis_interface.Sink raw_stream, // Raw byte stream
+    axis_interface.Source encoded_stream // Cobs encoded bytes
+);
+    axis_cobs_encode cobs_encoder (
+        .clk(raw_stream.clk),
+        .rst(raw_stream.rst),
+
+        // AXI input
+        .s_axis_tdata(raw_stream.tdata),
+        .s_axis_tvalid(raw_stream.tvalid),
+        .s_axis_tready(raw_stream.tready),
+        .s_axis_tlast(raw_stream.tlast),
+        .s_axis_tuser(raw_stream.tuser),
+
+        // AXI Output
+        .m_axis_tdata(encoded_stream.tdata),
+        .m_axis_tvalid(encoded_stream.tvalid),
+        .m_axis_tready(encoded_stream.tready),
+        .m_axis_tlast(encoded_stream.tlast),
+        .m_axis_tuser(encoded_stream.tuser)
+    );
+endmodule
+
+`default_nettype none
