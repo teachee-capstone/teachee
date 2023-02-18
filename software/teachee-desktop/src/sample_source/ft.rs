a77eaa941d2e246947b595f09fc1fb65679129dc
Eric Yang <e.yang@queensu.ca>
2023-02-18 15:29:58 -0500

    decode with cobs

--- software/teachee-desktop/src/sample_source/ft.rs
+++ software/teachee-desktop/src/sample_source/ft.rs
@@ -10,6 +10,7 @@ const MASK: u8 = 0xFF;
 const LATENCY_TIMER: Duration = Duration::from_millis(16);
 const IN_TRANSFER_SIZE: u32 = 0x10000;
 const RX_BUF_SIZE: usize = 100_000;
+const CHUNK_SIZE: usize = 4;
 
 /// A sample source that reads from a real TeachEE device.
 pub struct FtSampleSource {
@@ -28,6 +29,9 @@ impl SampleSource for FtSampleSource {
         ft.set_latency_timer(LATENCY_TIMER)?;
         ft.set_usb_parameters(IN_TRANSFER_SIZE)?;
         ft.set_flow_control_rts_cts()?;
+        ft.purge_rx()?;
+
+        thread::sleep(Duration::from_millis(10));
 
         Ok(Self {
             ft,
@@ -48,22 +52,37 @@ impl FtSampleSource {
         self.ft.read_all(&mut self.rx_buf[0..num_bytes])?;
         Ok(num_bytes)
     }
-    fn decode_and_copy(&self, channels: &mut Channels, num_bytes: usize) -> usize {
-        let start = self.rx_buf[0..num_bytes]
-            .iter()
-            .position(|&val| val == 0)
-            .unwrap();
-        let chunk_size = self.rx_buf[start + 1] as usize;
-        for (chunk, (v_sample, c_sample)) in self.rx_buf[start..num_bytes]
-            .chunks_exact(chunk_size)
-            .zip(zip(
-                channels.voltage1.iter_mut(),
-                channels.current1.iter_mut(),
-            ))
-        {
-            *v_sample = ((chunk[2] << 4) | chunk[3]) as f64 * 3.3 / 4095.0;
-            *c_sample = ((chunk[4] << 4) | chunk[5]) as f64 * 3.3 / 4095.0;
+    fn decode_and_copy(&mut self, channels: &mut Channels, num_bytes: usize) -> usize {
+        // Byte after the first 0
+        let start = self.rx_buf[..num_bytes].iter().position(|&x| x == 0).unwrap() + 1;
+        // Last 0
+        let end = self.rx_buf[..num_bytes].iter().rev().position(|&x| x == 0).unwrap();
+        // Check that we have at least one packet
+        assert_ne!(start - 1, end);
+        let mut cur_packet = start;
+        let mut channels_idx = 0;
+
+        while cur_packet < end {
+            let res = cobs::decode_in_place_report(&mut self.rx_buf[cur_packet..end]).unwrap();
+
+            // Copy the decoded chunks (of 4 bytes)
+            for (chunk, (v_sample, c_sample)) in self.rx_buf[cur_packet..(cur_packet + res.dst_used)]
+                .chunks_exact(CHUNK_SIZE)
+                .zip(zip(
+                    channels.voltage1[channels_idx..].iter_mut(),
+                    channels.current1[channels_idx..].iter_mut(),
+                ))
+            {
+                *v_sample = ((chunk[0] << 4) | chunk[1]) as f64 * 3.3 / 4095.0;
+                *c_sample = ((chunk[2] << 4) | chunk[3]) as f64 * 3.3 / 4095.0;
+            }
+
+            cur_packet += res.src_used + 1;
+            channels_idx += res.dst_used / CHUNK_SIZE;
+            assert_eq!(self.rx_buf[cur_packet - 1], 0);
+            assert_eq!(res.dst_used % CHUNK_SIZE, 0);
         }
-        (num_bytes - start) / chunk_size
+
+        channels_idx
     }
 }
