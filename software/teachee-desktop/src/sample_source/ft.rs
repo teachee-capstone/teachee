9c9095b933b5258ae484e2fac00e783c880d0cea
Eric Yang <e.yang@queensu.ca>
2023-02-20 15:42:27 -0500

    Custom decode

--- software/teachee-desktop/src/sample_source/ft.rs
+++ software/teachee-desktop/src/sample_source/ft.rs
@@ -10,7 +10,7 @@ const MASK: u8 = 0xFF;
 const LATENCY_TIMER: Duration = Duration::from_millis(16);
 const IN_TRANSFER_SIZE: u32 = 0x10000;
 const RX_BUF_SIZE: usize = 100_000;
-const CHUNK_SIZE: usize = 4;
+const PACKET_SIZE: usize = 6;
 
 /// A sample source that reads from a real TeachEE device.
 pub struct FtSampleSource {
@@ -53,36 +53,52 @@ impl FtSampleSource {
         Ok(num_bytes)
     }
     fn decode_and_copy(&mut self, channels: &mut Channels, num_bytes: usize) -> usize {
-        // Byte after the first 0
+        // Position after first 0
         let start = self.rx_buf[..num_bytes].iter().position(|&x| x == 0).unwrap() + 1;
-        // Last 0
-        let end = self.rx_buf[..num_bytes].iter().rev().position(|&x| x == 0).unwrap();
+        // Position after last 0
+        let end = num_bytes - self.rx_buf[..num_bytes].iter().rev().position(|&x| x == 0).unwrap();
         // Check that we have at least one packet
-        assert_ne!(start - 1, end);
-        let mut cur_packet = start;
-        let mut channels_idx = 0;
+        debug_assert_ne!(start, end);
+        debug_assert_eq!((end - start) % PACKET_SIZE, 0, "{start} {end}");
 
-        while cur_packet < end {
-            let res = cobs::decode_in_place_report(&mut self.rx_buf[cur_packet..end]).unwrap();
+        for (packet, (v_sample, c_sample)) in self.rx_buf[start..end]
+            .chunks_exact(PACKET_SIZE)
+            .zip(zip(
+                channels.voltage1.iter_mut(),
+                channels.current1.iter_mut(),
+            ))
+        {
+            debug_assert!(packet[0] < PACKET_SIZE as u8, "Unexpected block size: {}", packet[0]);
+            debug_assert_eq!(packet[5], 0);
+            if packet[0] == 5 {
+                // Fast path
+                *v_sample = ((packet[1] << 4) | packet[2]) as f64 * 3.3 / 4095.0;
+                *c_sample = ((packet[3] << 4) | packet[4]) as f64 * 3.3 / 4095.0;
+            } else {
+                let mut block = packet[0] - 1;
+                // Two bytes of packet overhead
+                let mut decoded: [u8; PACKET_SIZE - 2] = [0; PACKET_SIZE - 2];
+                let mut dst_index = 0;
 
-            // Copy the decoded chunks (of 4 bytes)
-            for (chunk, (v_sample, c_sample)) in self.rx_buf[cur_packet..(cur_packet + res.dst_used)]
-                .chunks_exact(CHUNK_SIZE)
-                .zip(zip(
-                    channels.voltage1[channels_idx..].iter_mut(),
-                    channels.current1[channels_idx..].iter_mut(),
-                ))
-            {
-                *v_sample = ((chunk[0] << 4) | chunk[1]) as f64 * 3.3 / 4095.0;
-                *c_sample = ((chunk[2] << 4) | chunk[3]) as f64 * 3.3 / 4095.0;
+                // Note: the packet index is just the dst_index + 1 (skip the first offset byte)
+                // Example packet: 01 02 22 02 44 00
+                // Decodes to:        00 22 00 44
+                while dst_index < decoded.len() {
+                    if block > 0 {
+                        decoded[dst_index] = packet[dst_index + 1];
+                    } else {
+                        decoded[dst_index] = 0;
+                        block = packet[dst_index + 1];
+                    }
+                    dst_index += 1;
+                    block -= 1;
+                }
+
+                *v_sample = ((decoded[0] << 4) | decoded[1]) as f64 * 3.3 / 4095.0;
+                *c_sample = ((decoded[2] << 4) | decoded[3]) as f64 * 3.3 / 4095.0;
             }
-
-            cur_packet += res.src_used + 1;
-            channels_idx += res.dst_used / CHUNK_SIZE;
-            assert_eq!(self.rx_buf[cur_packet - 1], 0);
-            assert_eq!(res.dst_used % CHUNK_SIZE, 0);
         }
 
-        channels_idx
+        (end - start) / PACKET_SIZE
     }
 }
