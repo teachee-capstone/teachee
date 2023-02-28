8555bf14c7d994638cd4b00d8678491417a527be
Ethan Peterson <17emp5@queensu.ca>
2023-02-27 23:00:20 -0500

    fix the bug in xadc_axis example

--- rtl/examples/xadc_axis/xadc_reader.py
+++ rtl/examples/xadc_axis/xadc_reader.py
@@ -9,6 +9,12 @@ with Device('FT84Y22T') as dev:
     # takes us from async to sync mode within ft245.
     # if you don't run this the clock does not come up
     dev.ftdi_fn.ftdi_set_bitmode(1, 0x40)
-    while(True):
-        # print(dev.read(1))
-        print(int.from_bytes(dev.read(1), "big"))
+    data = dev.read(20_000)
+    anomaly_counter = 0
+    for i in range(1, len(data)):
+        print(data[i])
+        if data[i - 1] == 0 and data[i] > 5:
+            anomaly_counter += 1
+
+    print("found %d anomalies" % anomaly_counter)
+
