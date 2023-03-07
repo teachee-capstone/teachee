aa8a4bdf6c0fa9f7101388dba3586e40b7138e4a
Eric Yang <e.yang@queensu.ca>
2023-03-07 10:43:51 -0500

    Add scaling/offsetting, also clean up gui
    Just use the provided sliders to scale and offset, which includes
    entering the values into a text box.
    Also remove unused code from GUI.
    
    Closes: #84
    Signed-off-by: Eric Yang <e.yang@queensu.ca>

--- software/teachee-desktop/src/app.rs
+++ software/teachee-desktop/src/app.rs
@@ -1,20 +1,12 @@
 use std::{
-    f64::consts::TAU,
     fmt,
+    ops::RangeInclusive,
     sync::{Arc, RwLock},
 };
 
 use eframe::egui::*;
 
 use crate::controller::{AppData, BufferState};
-
-#[derive(Debug, Clone, Copy, Default, PartialEq)]
-enum Channel {
-    #[default]
-    Off,
-    Sine,
-    Cos,
-}
 
 #[derive(Debug, Default)]
 enum TriggerControl {
@@ -32,29 +24,24 @@ impl fmt::Display for TriggerControl {
 
 const GROUP_SPACING: f32 = 3.0;
 const BUTTON_HEIGHT: f32 = 25.0;
-const TEXTEDIT_WIDTH: f32 = 30.0;
 
 // 1 MSPS
 const SAMPLE_PERIOD: f64 = 1e-6;
 
-#[derive(Debug, Default)]
+const H_SCALE_RANGE: RangeInclusive<f64> = RangeInclusive::new(0.1, 5.0);
+// TODO: use SAMPLE_PERIOD
+const H_OFFSET_RANGE: RangeInclusive<f64> = RangeInclusive::new(-5.0, 5.0);
+const V_SCALE_RANGE: RangeInclusive<f64> = RangeInclusive::new(0.1, 5.0);
+const V_OFFSET_RANGE: RangeInclusive<f64> = RangeInclusive::new(-5.0, 5.0);
+
+#[derive(Debug)]
 struct UIControls {
     h_offset: f64,
     h_scale: f64,
-    v_offset: f64,
-    v_scale: f64,
-    // TODO: use unused fields
-    _saved_v_offset: f64,
-    _saved_v_scale: f64,
-    _channel1_v_offset: f64,
-    _channel1_v_scale: f64,
-    _channel2_v_offset: f64,
-    _channel2_v_scale: f64,
-    h_scale_str: String,
-    channel1_v_scale_str: String,
-    channel2_v_scale_str: String,
-    channel1_on: bool,
-    channel2_on: bool,
+    channel1_v_offset: f64,
+    channel1_v_scale: f64,
+    channel2_v_offset: f64,
+    channel2_v_scale: f64,
     v_trigger_button_text: TriggerControl,
     v_trigger_threshold_text: String,
     v_trigger_format_wrong: bool,
@@ -63,13 +50,27 @@ struct UIControls {
     c_trigger_format_wrong: bool,
 }
 
+impl Default for UIControls {
+    fn default() -> Self {
+        Self {
+            h_offset: 0.0,
+            h_scale: 1.0,
+            channel1_v_offset: 0.0,
+            channel1_v_scale: 1.0,
+            channel2_v_offset: 0.0,
+            channel2_v_scale: 1.0,
+            v_trigger_button_text: TriggerControl::default(),
+            v_trigger_threshold_text: "".to_string(),
+            v_trigger_format_wrong: false,
+            c_trigger_button_text: TriggerControl::default(),
+            c_trigger_threshold_text: "".to_string(),
+            c_trigger_format_wrong: false,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct App {
-    flag: bool,
-    channel1: Channel,
-    channel1_offset: f64,
-    channel2: Channel,
-    channel2_offset: f64,
     data: AppData,
     buf_idx: usize,
     ui_controls: UIControls,
@@ -78,39 +79,11 @@ pub struct App {
 impl App {
     pub fn new(data: AppData) -> Self {
         Self {
-            flag: false,
-            channel1: Channel::default(),
-            channel1_offset: 0.0,
-            channel2: Channel::default(),
-            channel2_offset: 0.0,
             data,
             buf_idx: 0,
             ui_controls: UIControls::default(),
         }
     }
-    pub fn flip_flag(&mut self) {
-        self.flag = !self.flag
-    }
-}
-
-fn channel_control(ui: &mut Ui, label: &str, channel: &mut Channel, offset: &mut f64) {
-    CollapsingHeader::new(label)
-        .default_open(true)
-        .show(ui, |ui| {
-            ComboBox::from_label("Input")
-                .selected_text(format!("{channel:?}"))
-                .show_ui(ui, |ui| {
-                    ui.selectable_value(channel, Channel::Off, "Off");
-                    ui.selectable_value(channel, Channel::Sine, "Sin");
-                    ui.selectable_value(channel, Channel::Cos, "Cos");
-                });
-
-            ui.add(
-                Slider::new(offset, 0.0..=TAU)
-                    .show_value(false)
-                    .text("Offset"),
-            );
-        });
 }
 
 fn update_trigger(
@@ -164,11 +137,6 @@ impl eframe::App for App {
         ctx.request_repaint();
 
         let Self {
-            flag,
-            channel1,
-            channel1_offset,
-            channel2,
-            channel2_offset,
             data,
             buf_idx,
             ui_controls,
@@ -188,8 +156,6 @@ impl eframe::App for App {
                         frame.close();
                     }
                 });
-                ui.separator();
-                ui.label(format!("Flag: {}", *flag as i32));
             })
         });
 
@@ -210,13 +176,13 @@ impl eframe::App for App {
                         ui.columns(2, |uis| {
                             uis[0].label("Offset");
                             uis[0].add(
-                                Slider::new(&mut ui_controls.h_offset, 0.0..=100.0)
-                                    .show_value(false),
+                                Slider::new(&mut ui_controls.h_offset, H_OFFSET_RANGE)
+                                    .clamp_to_range(false),
                             );
                             uis[1].label("Scale");
                             uis[1].add(
-                                Slider::new(&mut ui_controls.h_scale, 0.0..=100.0)
-                                    .show_value(false),
+                                Slider::new(&mut ui_controls.h_scale, H_SCALE_RANGE)
+                                    .clamp_to_range(false),
                             );
                         });
 
@@ -224,30 +190,31 @@ impl eframe::App for App {
                         ui.separator();
                         ui.add_space(GROUP_SPACING);
 
-                        ui.label("Vertical");
-                        ui.group(|ui| {
-                            ui.vertical_centered_justified(|ui| {
-                                // TODO: save vertical offset/scale when checked and
-                                // update individual channel's vertical offset/scale
-                                // when unchecked
-                                ui.checkbox(&mut ui_controls.channel1_on, "Channel 1");
-                            });
-                        });
-                        ui.group(|ui| {
-                            ui.vertical_centered_justified(|ui| {
-                                ui.checkbox(&mut ui_controls.channel2_on, "Channel 2");
-                            });
-                        });
+                        ui.label("Channel 1 Vertical");
                         ui.columns(2, |uis| {
                             uis[0].label("Offset");
                             uis[0].add(
-                                Slider::new(&mut ui_controls.v_offset, 0.0..=100.0)
-                                    .show_value(false),
+                                Slider::new(&mut ui_controls.channel1_v_offset, V_OFFSET_RANGE)
+                                    .clamp_to_range(false),
                             );
                             uis[1].label("Scale");
                             uis[1].add(
-                                Slider::new(&mut ui_controls.v_scale, 0.0..=100.0)
-                                    .show_value(false),
+                                Slider::new(&mut ui_controls.channel1_v_scale, V_SCALE_RANGE)
+                                    .clamp_to_range(false),
+                            );
+                        });
+
+                        ui.label("Channel 2 Vertical");
+                        ui.columns(2, |uis| {
+                            uis[0].label("Offset");
+                            uis[0].add(
+                                Slider::new(&mut ui_controls.channel2_v_offset, V_OFFSET_RANGE)
+                                    .clamp_to_range(false),
+                            );
+                            uis[1].label("Scale");
+                            uis[1].add(
+                                Slider::new(&mut ui_controls.channel2_v_scale, V_SCALE_RANGE)
+                                    .clamp_to_range(false),
                             );
                         });
 
@@ -263,11 +230,6 @@ impl eframe::App for App {
                                 *ui_controls = UIControls::default();
                             }
                         });
-
-                        ui.add_space(GROUP_SPACING);
-                        // TODO: replace with new controls
-                        channel_control(ui, "Channel 1", channel1, channel1_offset);
-                        channel_control(ui, "Channel 2", channel2, channel2_offset);
                     });
 
                     ui.add_space(GROUP_SPACING);
@@ -299,56 +261,6 @@ impl eframe::App for App {
                 });
             });
 
-        TopBottomPanel::bottom("labels")
-            .resizable(false)
-            .show(ctx, |ui| {
-                ui.horizontal(|ui| {
-                    ui.vertical(|ui| {
-                        ui.horizontal(|ui| {
-                            ui.label("Channel 1:");
-                            if ui
-                                .add(
-                                    TextEdit::singleline(&mut ui_controls.channel1_v_scale_str)
-                                        .desired_width(TEXTEDIT_WIDTH),
-                                )
-                                .lost_focus()
-                            {
-                                // TODO: sync string with slider value
-                            }
-                            ui.label("V/div");
-                        });
-                        ui.horizontal(|ui| {
-                            ui.label("Channel 2:");
-                            if ui
-                                .add(
-                                    TextEdit::singleline(&mut ui_controls.channel2_v_scale_str)
-                                        .desired_width(TEXTEDIT_WIDTH),
-                                )
-                                .lost_focus()
-                            {
-                                // TODO: sync string with slider value
-                            }
-                            ui.label("V/div");
-                        });
-                    });
-
-                    ui.separator();
-
-                    ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
-                        if ui
-                            .add(
-                                TextEdit::singleline(&mut ui_controls.h_scale_str)
-                                    .desired_width(TEXTEDIT_WIDTH),
-                            )
-                            .lost_focus()
-                        {
-                            // TODO: sync string with slider value
-                        }
-                        ui.label("ms/div");
-                    });
-                });
-            });
-
         CentralPanel::default().show(ctx, |ui| {
             // Get the current buffer. Alternating between buffers is done by toggling
             // the buf_idx.
@@ -363,13 +275,13 @@ impl eframe::App for App {
             // Mapping i -> t using the fixed sample rate to get point (i * period, samples[i]).
             // TODO: Scale and offset
             let voltage = plot::Line::new(plot::PlotPoints::from_parametric_callback(
-                |i| (i * SAMPLE_PERIOD, channels.voltage1[i as usize]),
+                |i| (i * SAMPLE_PERIOD * ui_controls.h_scale + ui_controls.h_offset, channels.voltage1[i as usize] * ui_controls.channel1_v_scale + ui_controls.channel1_v_offset),
                 0.0..(num_samples as f64),
                 num_samples,
             ))
             .name("Channel 1");
             let current = plot::Line::new(plot::PlotPoints::from_parametric_callback(
-                |i| (i * SAMPLE_PERIOD, channels.current1[i as usize]),
+                |i| (i * SAMPLE_PERIOD * ui_controls.h_scale + ui_controls.h_offset, channels.current1[i as usize] * ui_controls.channel2_v_scale + ui_controls.channel2_v_offset),
                 0.0..(num_samples as f64),
                 num_samples,
             ))
