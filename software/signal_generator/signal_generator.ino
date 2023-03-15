2317f164987b5c43a6df32c262c0d76ee366f68f
Timothy Morland <timothy.john.morland@gmail.com>
2023-03-15 16:35:43 -0400

    Switch to easybutton

--- software/signal_generator/signal_generator.ino
+++ software/signal_generator/signal_generator.ino
@@ -1,38 +1,37 @@
+#include <EasyButton.h>
+
 #include "signals.h"
 
 #define X_PIN A0
 #define Y_PIN A1
-#define BUTTON_PIN 2
+#define BUTTON_PIN A2
 
 uint32_t t = 0;
 uint8_t f = 0;
 
-bool button_was_pressed = false;
+EasyButton button(BUTTON_PIN, true, true, false);
 
-bool button_toggled() {
-  bool button_is_pressed = digitalRead(BUTTON_PIN);
-  bool ret = !button_was_pressed && button_is_pressed;
-  button_was_pressed = button_is_pressed;
-  return ret;
-}
-
-void setup() {
-  analogWriteResolution(12);
-  pinMode(BUTTON_PIN, INPUT);
-}
-
-void loop() {
-  t += 1;
-  if (t >= NUM_TIMES) {
-    t = 0;
-  }
-
-  if (button_toggled()) {
+void handle_button() {
     f += 1;
 
     if (f >= NUM_FUNCS) {
       f = 0;
     }
+}
+
+void setup() {
+  button.begin();
+  button.onPressed(handle_button);
+
+  analogWriteResolution(12);
+}
+
+void loop() {
+  button.read();
+
+  t += 1;
+  if (t >= NUM_TIMES) {
+    t = 0;
   }
 
   uint32_t a = analogRead(A0) >> 4;
